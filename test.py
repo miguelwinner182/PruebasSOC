715ec3c1de94b21e94a3c0db46eff8fe91023b46
miguelwinner182 <miguelcubacardozo@gmail.com>
2024-05-03 11:54:33 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+# Definiendo las credenciales de prueba para el acceso a la base de datos PACIFICOTEST
+cadenas = [
+    'prima.com.pe',
+    'pacifico.com.pe',
+    'abcdelseguro.com.pe',
+    '35.241.2.28'
+]
+
+for cadena in cadenas:
+    for _ in range(4):
+        print(cadena)
